58253e67bb6bf8a2ab42f78dfec3a1504b12fa84
Gareth Dwyer <>
2021-05-13 13:00:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Lean Python the Right Way
---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+  /*window.onscroll = function () {
+    stick()
+  };
+
+  const navbar = document.getElementById("nav");
+  const sticky = navbar.offsetTop;
+
+  function stick() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }*/
+});
